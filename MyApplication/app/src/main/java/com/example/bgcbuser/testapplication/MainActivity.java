a26594471d5e40d43c9ca9e1649882fe390bb3d0
torrey1028 <rebecca.torrey@colorado.edu>
2015-08-13 19:23:39 -0700

    hello button

--- MyApplication/app/src/main/java/com/example/bgcbuser/testapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/bgcbuser/testapplication/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hello!!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
